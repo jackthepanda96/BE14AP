4253199982da4255628320c67012980a35a1f61a
Jerry Young <jerryyoung2911@gmail.com>
2023-01-03 14:15:33 +0700

    structure init

--- main.go
+++ main.go
@@ -8,43 +8,16 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-// fmt.Println("Run server....")
-//
-//	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-//		if r.Method == "GET" {
-//			w.Header().Set("content-type", "application/json")
-//			w.Write([]byte("Hello world"))
-//		}
-//	})
-//
-// err := http.ListenAndServe(":8000", nil)
-//
-//	if err != nil {
-//		log.Println("Start server error")
-//	}
 func main() {
 	e := echo.New()
 	arr := []user.User{}
+	controll := user.UserControll{ListUser: arr}
+
 	e.GET("/hello", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, "Hello World")
 	})
-	e.POST("/users", func(c echo.Context) error {
-		tmp := user.User{}
-		err := c.Bind(&tmp)
-		if err != nil {
-			return c.JSON(http.StatusBadRequest, "format inputan salah")
-		}
-
-		arr = append(arr, tmp)
-
-		return c.JSON(http.StatusCreated, "user berahsil di tambahkan")
-	})
-	e.GET("/users", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, map[string]interface{}{
-			"data":    arr,
-			"message": "berhasil menampilkan daftar user",
-		})
-	})
+	e.POST("/users", controll.Insert())
+	e.GET("/users", controll.GetAllUser())
 	if err := e.Start(":8000"); err != nil {
 		log.Println(err.Error())
 	}
