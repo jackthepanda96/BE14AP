46bda3d2d495197563474dc3795f7320aa919219
Jerry Young <jerryyoung2911@gmail.com>
2023-01-09 14:11:15 +0700

    unit testing mockery

--- features/user/services/service.go
+++ features/user/services/service.go
@@ -7,7 +7,6 @@ import (
 	"errors"
 	"log"
 	"strings"
-	"time"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/golang-jwt/jwt"
@@ -40,13 +39,13 @@ func (uuc *userUseCase) Login(email, password string) (string, user.Core, error)
 
 	if err := bcrypt.CompareHashAndPassword([]byte(res.Password), []byte(password)); err != nil {
 		log.Println("login compare", err.Error())
-		return "", user.Core{}, errors.New("password tidak sesuai")
+		return "", user.Core{}, errors.New("password tidak sesuai " + res.Password)
 	}
 
 	claims := jwt.MapClaims{}
 	claims["authorized"] = true
 	claims["userID"] = res.ID
-	claims["exp"] = time.Now().Add(time.Hour * 1).Unix() //Token expires after 1 hour
+	// claims["exp"] = time.Now().Add(time.Hour * 1).Unix() //Token expires after 1 hour
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	useToken, _ := token.SignedString([]byte(config.JWT_KEY))
 
@@ -55,11 +54,13 @@ func (uuc *userUseCase) Login(email, password string) (string, user.Core, error)
 }
 func (uuc *userUseCase) Register(newUser user.Core) (user.Core, error) {
 	hashed, err := bcrypt.GenerateFromPassword([]byte(newUser.Password), bcrypt.DefaultCost)
+
 	if err != nil {
 		log.Println("bcrypt error ", err.Error())
 		return user.Core{}, errors.New("password process error")
 	}
 	newUser.Password = string(hashed)
+	// log.Panic(string(hashed))
 	res, err := uuc.qry.Register(newUser)
 	if err != nil {
 		msg := ""
--- features/user/services/service_test.go
+++ features/user/services/service_test.go
@@ -1,1 +1,111 @@
 package services
+
+import (
+	"api/features/user"
+	"api/mocks"
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+var (
+	pass string
+)
+
+func TestRegister(t *testing.T) {
+	data := mocks.NewUserData(t)
+	inputData := user.Core{Nama: "jerry", Email: "jerr@alterra.id", Alamat: "pasuruan", HP: "08123456", Password: "be1422"}
+	resData := user.Core{ID: uint(1), Nama: "jerry", Email: "jerr@alterra.id", Alamat: "pasuruan", HP: "08123456"}
+	data.On("Register", mock.Anything).Return(resData, nil).Once()
+	srv := New(data)
+	res, err := srv.Register(inputData)
+	assert.Nil(t, err)
+	assert.Equal(t, resData.ID, res.ID)
+	assert.Equal(t, resData.Nama, res.Nama)
+	data.AssertExpectations(t)
+	// srv := New(&mockUserData{})
+	// t.Run("Berhasil register", func(t *testing.T) {
+	// 	res, err := srv.Register(inputData)
+	// 	pass = res.Password
+	// 	assert.Nil(t, err)
+	// 	assert.Greater(t, res.ID, uint(0))
+	// 	// assert.Equal(t, "", res.Password, "")
+	// })
+}
+
+func TestLogin(t *testing.T) {
+	srv := New(&mockUserData{})
+	t.Run("Berhasil login", func(t *testing.T) {
+		token, res, err := srv.Login("jerry@alterra.id", "be1422")
+		assert.Nil(t, err, "err seharusnya nil")
+		assert.Greater(t, res.ID, uint(0), "id seharusnya lebih dari 0")
+		assert.Equal(t, res.Nama, "jerry")
+		assert.NotEmpty(t, token, "seharusnya token tidak kosong")
+	})
+
+	t.Run("Password error", func(t *testing.T) {
+		srvE := New(&mockUserDataError{})
+		token, res, err := srvE.Login("jerry@alterra.id", "be1422")
+		assert.ErrorContains(t, err, "server")
+		assert.Empty(t, token)
+		assert.Equal(t, uint(0), res.ID)
+
+	})
+}
+
+type mockUserData struct{}
+
+func (mud *mockUserData) Login(email string) (user.Core, error) {
+	return user.Core{
+		ID:       1,
+		Nama:     "jerry",
+		Email:    "jerry@alterra.id",
+		Alamat:   "pasuruan",
+		HP:       "0812345678",
+		Password: "$2a$10$r8lNLijRdIE3nZpVh60oT.LA.rpl7G9ONQ9RiLCD4fxqZoQLxHIEW",
+	}, nil
+}
+func (mud *mockUserData) Register(newUser user.Core) (user.Core, error) {
+	return user.Core{
+		ID:     1,
+		Nama:   "jerry",
+		Email:  "jerry@alterra.id",
+		Alamat: "pasuruan",
+		HP:     "0812345678",
+	}, nil
+}
+func (mud *mockUserData) Profile(id uint) (user.Core, error) {
+	return user.Core{
+		ID:     1,
+		Nama:   "jerry",
+		Email:  "jerry@alterra.id",
+		Alamat: "pasuruan",
+		HP:     "0812345678",
+	}, nil
+}
+
+type mockUserDataError struct{}
+
+func (mude *mockUserDataError) Login(email string) (user.Core, error) {
+	return user.Core{}, errors.New("terdapat masalah pada server")
+}
+func (mude *mockUserDataError) Register(newUser user.Core) (user.Core, error) {
+	return user.Core{
+		ID:     1,
+		Nama:   "jerry",
+		Email:  "jerry@alterra.id",
+		Alamat: "pasuruan",
+		HP:     "0812345678",
+	}, nil
+}
+func (mude *mockUserDataError) Profile(id uint) (user.Core, error) {
+	return user.Core{
+		ID:     1,
+		Nama:   "jerry",
+		Email:  "jerry@alterra.id",
+		Alamat: "pasuruan",
+		HP:     "0812345678",
+	}, nil
+}
